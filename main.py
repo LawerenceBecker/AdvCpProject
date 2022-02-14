51bbb262f9f09640a5aff4c414082de8e3ebcf54
Lawerence Becker <95592713+LawerenceBecker@users.noreply.github.com>
2022-02-14 15:34:15 +0000

    Added basic pygame bolierplate

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("I, Noah Espinoza, wrote this print statement to commit to the Github repository.")
-print('Rat King')
-print('hello world')
+import pygame
+import sys
+
+screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption('Adv CP Pokemon Clone')
+clock = pygame.time.Clock()
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT
+                sys.exit()
+
+        screen.fill('#9edb64')
+
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
