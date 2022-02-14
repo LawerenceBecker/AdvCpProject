6d08748ade805f090fc887f0c90e07657f3236af
carlsagan <97457166+NoahEspi@users.noreply.github.com>
2022-02-14 18:35:35 +0000

    Move quicker with shift

--- main.py
+++ main.py
@@ -15,9 +15,14 @@ image = moveUp;
 x = 100;
 y = 100;
 
+speed = 5;
+
 def main():
   global image, x, y
-  
+
+  prevTick = pygame.time.get_ticks()
+
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -26,18 +31,29 @@ def main():
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP]:
-      y += -5;
-      image = moveUp;
-    elif keys[pygame.K_DOWN]:
-      y += 5;
-      image = moveDown;
-    elif keys[pygame.K_LEFT]:
-      x += -5;
-      image = moveLeft;
-    elif keys[pygame.K_RIGHT]:
-      x += 5;
-      image = moveRight;
+    if keys[pygame.K_LSHIFT]:
+      moveTimer = 100
+    else: 
+      moveTimer = 250
+
+    if pygame.time.get_ticks() - prevTick >= moveTimer:
+      
+        if keys[pygame.K_UP]:
+            y += -64;
+            image = moveUp;
+            prevTick = pygame.time.get_ticks()
+        elif keys[pygame.K_DOWN]:
+            y += 64;
+            image = moveDown;
+            prevTick = pygame.time.get_ticks()
+        elif keys[pygame.K_LEFT]:
+            x += -64;
+            image = moveLeft;
+            prevTick = pygame.time.get_ticks()
+        elif keys[pygame.K_RIGHT]:
+            x += 64;
+            image = moveRight;
+            prevTick = pygame.time.get_ticks()
 
     screen.fill('#9edb64')
     screen.blit(image, (x, y));
