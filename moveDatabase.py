c5a8a38aa855020be919ca1675b692513e718519
carlsagan <97457166+NoahEspi@users.noreply.github.com>
2022-03-02 03:40:14 +0000

    Made move information accurate to the real game

--- moveDatabase.py
+++ moveDatabase.py
@@ -1,54 +1,67 @@
+# This website provides information for all available pokemon go moves:
+# https://gamepress.gg/pokemongo/pokemon-move/ember
+
+# (FillMeter is the Energy Delta stat)
+# fill meter should equal 10 energy per second for most moves (EPS is not the same as Energy Delta)
+
+
+# For how much each special move needs to be charged, it's based on it's Charge Energy column
+# 3 means it needs 33 energy, 2 means it needs 50, 1 means 100
+## the more energy it needs the more damage it does
+
+## found here: https://pokemongohub.net/post/meta/multi-bar-vs-single-bar-charged-moves/
+
 moves = {'Quick': {
           'Fire': {
-            'Ember': {
-              'Speed': 4, 
-              'Power': 4,
-              'Cooldown': 500,
+            'Ember': { 
+              'Power': 10,
+              'Cooldown': 1000,
               'FillMeter': 10}
           },
           'Grass': {
             'Vine Whip': {
-              'Speed': 4,
-              'Power': 4,
-              'Cooldown': 500,
-              'FillMeter': 10}
+              'Power': 7,
+              'Cooldown': 600,
+              'FillMeter': 6}
           },
           'Water': {
             'Water Gun': {
-              'Speed': 4,
-              'Power': 4,
+              'Power': 5,
               'Cooldown': 500,
-              'FillMeter': 10}
+              'FillMeter': 5}
           },
           'Normal': {
             'Tackle': {
-              'Speed': 4,
-              'Power': 4,
+              'Power': 5,
               'Cooldown': 500,
-              'FillMeter': 10}
+              'FillMeter': 5}
           }
 },
          
         'Special': {
          'Fire': {
            'Fire Punch': {
-             'Power': 8,
-             'MaxMeter': 100}
+             'Power': 55,
+             'Bars': 3,
+             'MeterSize': 33}
          },
          'Grass': {
            'Solar Beam': {
-             'Power': 8,
+             'Power': 180,
+             'Bars': 1,
              'MaxMeter': 100}
          },
          'Water': {
            'Surf': {
-             'Power': 8,
-             'MaxMeter': 100}
+             'Power': 65,
+             'Bars': 2
+             'MaxMeter': 50}
          },
          'Normal': {
            'Return': {
-             'Power': 8,
-             'MaxMeter': 100}
+             'Power': 35,
+             'Bars': 3,
+             'MaxMeter': 33}
          }
         }
       }
